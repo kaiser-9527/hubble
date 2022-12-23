0cc9bc76553a6f84b9243ea33c2acfd9673973f0
kaiser <1003738200@qq.com>
2022-12-23 15:55:42 +0800

    chore: revert

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,13 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { TUser } from "~/types/common";
 
-const url = import.meta.env.DEV
-  ? import.meta.env.VITE_SUPABASE_URL
-  : process.env.SUPABASE_URL;
-const key = import.meta.env.DEV
-  ? import.meta.env.VITE_SUPABASE_ANON_KEY
-  : process.env.SUPABASE_ANON_KEY;
-export const supabase = createClient(url, key);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
 export const getSupabaseUser = (callback: (user?: TUser) => void) => {
   supabase.auth.getSession().then(({ data }) => {
